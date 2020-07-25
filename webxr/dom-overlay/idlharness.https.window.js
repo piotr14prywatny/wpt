99a10d2985ff7c865448799b90b353920531c9cb
Klaus Weidner <klausw@chromium.org>
2020-07-24 17:20:12 -0700

    Fix onbeforexrselect event handler inheritance
    The onbeforexrselect event is supposed to be part of GlobalEventHandlers
    and separately inherited by {HTML,MathML,SVG}Element, Window, and
    Document. The current implementation had added it at the Element
    level which isn't correct, causing test failures in the idlharness
    test.
    
    Update the implementation to fix the inheritance, which unfortunately
    adds a bit of extra boilerplate.
    
    Also add objects to the idlharness test to verify the inheritance.
    
    Spec: https://immersive-web.github.io/dom-overlays/#onbeforexrselect
    
    Fixed: 1108685
    Change-Id: I8df73304b7b2d9bd93df30b78953aa9b378f8cad

--- webxr/dom-overlay/idlharness.https.window.js
+++ webxr/dom-overlay/idlharness.https.window.js
@@ -7,10 +7,22 @@
 
 idl_test(
   ['dom-overlays'],
-  ['webxr', 'html', 'dom'],
+  ['webxr', 'html', 'dom', 'SVG'],
   async idl_array => {
+    self.svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    document.body.appendChild(svgElement);
+    // There currently isn't a MathML definition for idlharness,
+    // use a minimal definition to verify that the inheritance of
+    // GlobalEventHandlers works.
+    idl_array.add_idls("interface MathMLElement : Element {}; MathMLElement includes GlobalEventHandlers;")
+    self.mathmlElement = document.createElementNS('http://www.w3.org/1998/Math/MathML', 'math');
+    document.body.appendChild(mathmlElement);
     idl_array.add_objects({
-      // TODO: Add object instances
+      Document: ['document'],
+      HTMLElement: ['document.body'],
+      MathMLElement: ['mathmlElement'],
+      SVGElement: ['svgElement'],
+      Window: ['window']
     });
   }
 );
